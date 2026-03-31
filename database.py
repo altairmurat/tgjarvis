74b20eb9cc4e21a3a08bfe03ac2eba74852b8913
altairmurat <murat.altair.didaruly@gmail.com>
2026-03-31 17:09:56 +0900

    fix: add pool_pre_ping for supabase pooler

--- database.py
+++ database.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from env import DATABASE_URL
+from sqlalchemy.orm import sessionmaker
+import os
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"options": "-c statement_timeout=10000"},
+    pool_pre_ping=True
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
